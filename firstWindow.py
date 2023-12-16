2fe357387952eeb85137ef0ea162a5046aec17a7
rishabhdv1 <rishabh.bairagi.136@gmail.com>
2023-12-16 11:36:38 +0530

    new

---
+++ firstWindow.py
@@ -0,0 +1,11 @@
+import sys
+# from topmodual.submodual import elem1,elem2,elem3
+from PyQt6.QtWidgets import QApplication,QWidget
+
+
+
+nmn = QApplication([]) # Passing an empty list
+window = QWidget()
+window.show()
+
+nmn.exec()
